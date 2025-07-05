42d982abf4bbd7167e42309cde4657cceab22596
amandamccarvill <anmccarvill@gmail.com>
2025-07-05 08:35:49 -0700

    added environment variable to the client

--- client/src/api.js
+++ client/src/api.js
@@ -1,4 +1,5 @@
-const BASE_URL = 'http://localhost:4000/api';
+// const BASE_URL = 'http://localhost:4000/api';
+const BASE_URL = import.meta.env.VITE_API_KEY
 
 export async function api(path, method = 'GET', body = null, token = null) {
   const res = await fetch(`${BASE_URL}${path}`, {
